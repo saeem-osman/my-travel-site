d31ee3698386894db1ba91c107fea2e54f34fdea
Saeem Osman <saeem.osman@gmail.com>
2018-12-12 00:45:20 +0600

    Completed reveal on scroll section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,8 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".featurte-item"), "85%");
+new RevealOnScroll($(".testimonials"), "60%");
